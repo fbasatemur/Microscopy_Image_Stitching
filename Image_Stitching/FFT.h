e49d69ad6322ee25ba47c59f2db55fb1559d2e09
fbasatemur <fatihbasatemur@gmail.com>
2021-05-06 16:19:21 +0300

    deleted main branch project

--- Image_Stitching/FFT.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-void fft(int n, double* xRe, double* xIm, double* yRe, double* yIm);
-void ifft(int n, double* xRe, double* xIm, double* yRe, double* yIm);
--- Image_Stitching/xy.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-typedef struct { int x, y; } xy;
